2eda5ce7c527366f0b573c97919e2bbc72c134db
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2025-01-15 05:26:49 -0500

    feat: added some extra packages like tailwind.css font awesome. Started setting up routes and the progress has been set up
    maint:rebasing the branch

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,14 @@ const router = createBrowserRouter([
     element:(
     <>
     <Header />
+    <MenuCreationBlock/>
+    <ProgressBar />
     <CalendarPage />,
     < Footer/>
     </>
     )
   },
   
-   
 ]);
 
 function App() {
