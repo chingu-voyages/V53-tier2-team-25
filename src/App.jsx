db06aab83d2ab2100b31e1f641030c723d95b524
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2025-01-15 05:26:49 -0500

    feat: added some extra packages like tailwind.css font awesome. Started setting up routes and the progress has been set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import "./App.css";
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import CalendarPage from "./pages/Calendar-Page.jsx";
+import Header from "./components/header.jsx";
+import Footer from "./components/footer.jsx";
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+  },
+  {
+    path: "/calendar-page",
+    element:(
+    <>
+    <Header />
+    <CalendarPage />,
+    < Footer/>
+    </>
+    )
+  },
+  
+   
+]);
 
 function App() {
-  return (
-    <>
-      <div>app.jsx</div>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
