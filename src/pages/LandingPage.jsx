d203f1bca7cd915956874551c5de9ca839987d2b
Prerna Lele <prernalele@gmail.com>
2025-01-15 15:51:23 -0500

    feat: adds hero image header footer

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Header from "../components/header";
+import Footer from "../components/footer.jsx";
+import LandingPageBackground from "../../public/LandingPageBackground.png";
+import DesktopHeroImage from "../../public/DesktopHeroImage.png";
 
 const LandingPage = () => {
-  return <div>Landing page</div>;
+  return (
+    <div>
+      <Header />
+      <img src={LandingPageBackground}></img>
+      <div className="ml-1/12 w-7/12 -mt-1">
+        <img src={DesktopHeroImage}></img>
+      </div>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default LandingPage;
