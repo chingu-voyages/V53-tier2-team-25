78d3e31ba0fa7b918972f5475645b7211a98549a
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2025-01-17 01:03:24 -0500

    fixes: adding logo to header and changing the color hover for current say

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,14 @@
+import PlatepalLogo from '../assets/images/Platepal-Logo.svg'; 
+
+
 const Header = () => {
   return (
     <header className="bg-[#528540] text-white p-3 flex justify-between items-center">
       <div className="flex items-center w-full sm:justify-start justify-between">
-        <div className="w-14 h-14 bg-yellow-500 rounded-full flex items-center justify-center text-xl font-bold">
-          {/* Logo or Icon */}
+        <div className="w-14 h-14  rounded-full flex items-center justify-center text-xl font-bold">
+          <img src={PlatepalLogo} alt="Platepal Logo" />
         </div>
-        <span className="ml-6 text-4xl font-semibold">Plate Pal</span>
+        <span className="ml-6 text-4xl font-semibold">PlatePal</span>
       </div>
     </header>
   );
