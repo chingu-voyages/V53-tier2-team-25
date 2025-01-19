d1212e4586d96a1ee7a850b9e37d1c0a8f69dc29
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2025-01-18 19:15:02 -0500

    adding svg image to tab

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,10 +5,10 @@ const Header = () => {
   return (
     <header className="bg-[#528540] text-white p-3 flex justify-between items-center">
       <div className="flex items-center w-full sm:justify-start justify-between">
-        <div className=" rounded-full flex items-center justify-center text-xl font-bold">
-          <img className='h-15' src={PlatepalLogo} alt="Platepal Logo" />
+        <div className="h-14 w-14 rounded-full flex items-center justify-center text-xl font-bold">
+          <img className='h-13' src={PlatepalLogo} alt="Platepal Logo" />
         </div>
-        <span className="ml-6 text-5xl font-poppins font-semibold text-neutral-l-ight ">PlatePal</span>
+        <span className="ml-6 text-4xl font-poppins font-semibold text-neutral-l-ight ">PlatePal</span>
       </div>
    
     </header>
