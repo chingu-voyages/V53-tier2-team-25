7ca48ba8d1843981a134151bdef5f7320a5046b4
S. Ryan <sokuenryan@gmail.com>
2025-01-24 19:16:38 -0500

    initial commit: added dish-select component file

---
+++ src/components/dish-select.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DishSelect = () => {
+  return (
+    <div className='dish-select'>
+      <h1>Change or remove dishes based on your preferences</h1>
+    </div>
+  );
+};
+
+export default DishSelect;
