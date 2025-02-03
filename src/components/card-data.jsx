264e860f0ccb4d454c9989e83a84c0f9dcd033fb
S. Ryan <sokuenryan@gmail.com>
2025-02-03 16:55:31 -0500

    modified card-data.jsx file

--- src/components/card-data.jsx
+++ src/components/card-data.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 const Card = ({ day }) => {
   return (
     <div className="card-wrapper flex flex-col justify-between h-full">
-        <div className='card-header: card-data flex justify-end p-2'>
-            <h2 className='font-bold '>{day}</h2>
-            <button className='ml-8 rounded-[20px] bg-gray-400 h-[20px] w-[20px] text-white font-bold'>X</button>
+        <div className='card-header: card-data w-full flex p-2'>
+            <div className='day: flex justify-center'><h2 className='font-bold'>{day}</h2></div>
+            <div className='exit-btn'><button className='rounded-[20px] bg-gray-400 h-[20px] w-[25px] text-white font-bold'>X</button></div>
+            
         </div>
 
         <div className="card-data bg-blue-300 flex justify-center items-center h-full">
@@ -13,7 +14,7 @@ const Card = ({ day }) => {
         </div>
 
         <div className="card-footer: flex justify-center h-10 w-full p-2">
-            <button className='bg-[#528540] text-white rounded-lg w-40'>Complete Menu</button>
+            <button className='bg-[#528540] text-white rounded-lg w-32'>Change Dish</button>
         </div>
     </div>
   );
