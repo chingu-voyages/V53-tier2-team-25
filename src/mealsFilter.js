9f98b717b3fcebd02f61979ec222d7478a1e4f42
Prerna Lele <prernalele@gmail.com>
2025-02-06 14:52:09 -0500

    feat: successfully filters out allergy causing recipes

--- src/mealsFilter.js
+++ src/mealsFilter.js
@@ -5,18 +5,48 @@ const mealsFilter = (allergies, numberOfDays) => {
   // step 1 : knowing selected allergies with tags
   // steps 2: combine all the tags in one array
   // step 3: iterate over each recipe
-  // step 4: filter (some) out the recipes that MATCH any ONE allergy causing tag
+  // step 4: filter (any) out the recipes that MATCH any ONE allergy causing tag
   // step 5: return the recipes that don't match any of the allergy tags (combined array)
+
+  //Combined all allergy tags into 2D array
   let allAllergyTags = allergies.map((allergy) => {
     if (allergy.isSelected) {
-      console.log("allergyTag", allergy.allergyTag);
+      console.log("allergy.allergyTag", allergy.allergyTag);
       return allergy.allergyTag;
     }
   });
+
+  // filters out the undefined entries
   const allergyTagsWithoutUndefined = allAllergyTags.filter((tag) => !!tag);
-  const finalFlattenedAllergyTags = [].concat(...allergyTagsWithoutUndefined);
-  console.log("allergyTagsWithoutUndefined", allergyTagsWithoutUndefined);
-  console.log("finalFlattenedAllergyTags", finalFlattenedAllergyTags);
+
+  //flattens the 2D Array into single array of all tags
+  const flattenedAllergyTags = [].concat(...allergyTagsWithoutUndefined);
+
+  // console.log("flattenedAllergyTags", flattenedAllergyTags);
+
+  const selectedRecipes = apiResponse.recipes.filter(
+    ({ ingredients }) => {
+      const areIngredientsAllergyCausing = ingredients.filter(
+        (oneIngredient) => {
+          console.log(
+            `Does ${oneIngredient} match any of allergies`,
+            flattenedAllergyTags.includes(oneIngredient.toLowerCase())
+          );
+          return flattenedAllergyTags.includes(oneIngredient.toLowerCase());
+        }
+      );
+      console.log(
+        "areIngredientsAllergyCausing",
+        areIngredientsAllergyCausing.length
+      );
+      return areIngredientsAllergyCausing.length == 0 ? true : false;
+    }
+    //now that we have list of ingredients we map over them
+  );
+
+  console.log("selected Recipes", selectedRecipes);
+
+  // console.log("selected Recipes", selectedRecipes);
 
   // const result = apiResponse.recipes.map((eachItem) => {
   //   console.log("eachItem.ingredients", eachItem.ingredients);
